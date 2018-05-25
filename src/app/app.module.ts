54a90133804553d2c3c23230c7fb23856fbd63b9
Rajesh Vijayan <A-8076@ibsplc.com>
2018-05-25 16:23:48 +0530

    ng material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatSnackBarModule, MatInputModule, MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages.component';
@@ -9,7 +11,7 @@ import { MessagesComponent } from './messages.component';
     AppComponent, MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MatButtonModule, MatCardModule,MatSnackBarModule, MatInputModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/messages.component.ts
+++ src/app/messages.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'messages',
-  template: ` <div *ngFor="let message of messages">{{message.text}} By: {{message.owner}}
+  template: `    
     
-  </div> `
+    <div *ngFor="let message of messages">
+      <mat-card style="margin: 8px">
+        <mat-card-title> {{message.owner}} </mat-card-title>
+        <mat-card-content> {{message.text}} </mat-card-content> 
+      </mat-card>  
+    </div> 
+            
+  `
 })
 
 export class MessagesComponent{
